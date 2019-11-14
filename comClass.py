185d137a1d32191ebf06eb88512d3e04d7214f4f
eregar <30888167+eregar@users.noreply.github.com>
2019-11-14 12:42:14 -0600

    ver 0.3.0 dialing

--- comClass.py
+++ comClass.py
@@ -1,6 +1,7 @@
 import constants
 import serial
 import time
+import threading
 class Com(object):
     
     def __init__(self,comNumber: int):
@@ -8,25 +9,103 @@ class Com(object):
         self.comNumber=comNumber
         self.status=constants.OFFLINE
         self.puerto=serial.Serial()
-    
-    def startSerial(self):
-        self.puerto=serial.Serial('COM'+str(self.comNumber),baudrate=constants.BAUDRATE,
-            timeout=constants.WAITINGTIME)
-        self.send(b"AT\r\n")
-        self.read(2)
-        self.status=constants.OK
 
-    def dial(self,numero: str):
-        self.send(b'ATD'+numero+'\r\n')
-        self.read(2)
-        time.sleep(5)
+
+    def autoAnswer(self,origen):
+        try:
+            while True:
+                bytesToRead=self.puerto.inWaiting()
+                if bytesToRead!=0:
+                    x=self.read(bytesToRead)
+                    if 'RING' in str(x):
+                        self.send(b'ATA\r\n')
+                    elif 'CARRIER' in str(x):
+                        print('me colgo')
+                        self.changeState(constants.OK)
+                        break
+                    else:
+                        print(x)
+                time.sleep(1)
+        except serial.SerialException:
+            self.puerto.close()
+            self.changeState(constants.OFFLINE)
+    
+    def hang(self,tiempo):
+        time.sleep(tiempo)
         self.send(b'ATH\r\n')
+        self.read()
+        self.changeState(constants.OK)
+
+    def ans(self,origen):
+        self.startSerial()
+        if self.status==constants.OK:
+            answer=threading.Thread(target=self.autoAnswer, daemon=True)
+            answer._args=(origen,)
+            answer.start()
+            self.changeState(constants.DIALING)
+            return True
+        else:
+            return False
+    
+    def zoneSerial(self):
+        x=threading.Thread(target=self.startSerial,daemon=True)
+        x.start()
+
+    def startSerial(self):
+        try:
+            self.puerto.close()
+            self.changeState(constants.OFFLINE)
+            self.puerto=serial.Serial('COM'+str(self.comNumber),baudrate=constants.BAUDRATE,
+                timeout=constants.WAITINGTIME)
+        except serial.serialutil.SerialException:
+            pass
+        else:
+            self.send(b"AT\r\n")
+            if('OK' in str(self.read(6))):
+                self.changeState(constants.OK)
+
+    def dial(self,numero: str,seconds: int):
+        self.startSerial()
+        if self.status==constants.OK:
+            self.send(b'ATD'+numero.encode("utf-8")+b';\r\n')
+            if('OK' in self.read()):
+                self.changeState(constants.DIALING)
+            else:
+                return False
+            print('done waiting')
+            calling=threading.Thread(target=self.hang,args=(60),daemon=True)
+            calling._args=(seconds+10,)
+            calling.start()
+            return True
+        else:
+            return False
+    
+    def changeState(self,estado: str):
+        #signalChange(self,estado)
+        self.status=estado
         
     def send(self, msg: bytes):
         self.puerto.write(msg)
 
-    def read(self,byteNumbers):
-        return self.puerto.read(byteNumbers)
+    def read(self,byteNumber=0):
+        if byteNumber==0:
+            buffer=''
+            timeout=0
+            time.sleep(0.2)
+            while timeout<=constants.WAITINGTIME:
+                byteNumber=self.puerto.inWaiting()
+                if byteNumber!=0:
+                    buffer+=str(self.puerto.read(byteNumber))
+                if'OK' in buffer:
+                    break
+                timeout+=1
+                time.sleep(1)
+            if buffer=='':
+                self.changeState(constants.OFFLINE)
+                self.puerto.close()
+            return buffer
+        else:
+            return self.puerto.read(byteNumber)
     
     def getIMEI(self):
         pass
--- communicador.py
+++ communicador.py
@@ -1,49 +1,94 @@
-import serial
 import tkinter
-import threading
+from tkinter.messagebox import showinfo
 import time
 import constants
 import comClass
+import threading
 
 #variables globales
 puertos=[]
-butons=[]
+statuses=[]
+telefonos=[]
+checkBoxes=[]
+
 app=tkinter.Tk(className="AT reciever")
+writeFrame=tkinter.Frame(app)
+comFrame=tkinter.Frame(app)
+telFrame=tkinter.Frame(app)
+chosen='3315528889'
+minutos=60
 
+def startPorts():
+    for puerto in range(len(puertos)):
+        puertos[puerto].zoneSerial()
+        statuses[puerto]['text']=puertos[puerto].status
+def crossDial():
+    print('calling')
+    temp=[]  
+    for x in range(len(checkBoxes)):
+        if checkBoxes[x].get()!=0:
+            temp.append(x)
+    print(len(temp)//2)
+    for puerto in range(len(temp)//2):
+        print('metsuketaaa')
+        half=puerto+len(temp)//2
+        #statuses[temp[puerto]]['text']=constants.DIALING
+        s=puertos[temp[puerto]].dial(telefonos[temp[half]].get(),minutos)
+        if s:
+            #statuses[temp[half]]['text']=constants.RECIEVING
+            r=puertos[temp[half]].ans('3315528889')
+            if not r:
+                puertos[temp[puerto]].hang(0)
+                #statuses[temp[half]]['text']=constants.ERROR
+            #statuses[temp[puerto]]['text']=constants.ERROR
+    if len(temp) % 2!=0:
+        indexsobrante=temp[-1]
+        puertos[indexsobrante].dial(chosen,minutos)
+    print('done')
 
-def COMThread(comNumber):
-    pass
+def constantCheck():
+    while True:
+        for puerto in range(len(puertos)):
+            statuses[puerto]['text']=puertos[puerto].status
+        time.sleep(1)
 
-def test(numero):
-    print("quiero el lock alvv")
-    printear(numero)
-    print("ganeee uwu")
-
-def printear(variable):
-    #print(testo2.get(1.0,tkinter.END))
-    loc.acquire()
-    print(variable)
-    time.sleep(1)
-    loc.release()
 
 for port in constants.PORTNUMBERS:
     puertos.append(comClass.Com(port))
-    #puertos.append(threading.Thread(target=test,args=(port),daemon=True))
-    butons.append(tkinter.Label(master=app,text=str(port)))
+    statuses.append(tkinter.Label(master=comFrame,text=str(port)))
 
-loc=threading.Lock()
-#w=tkinter.Canvas(m,width=100,height=100)
-#boton1= tkinter.Button(app,text='stop',width=25,command=app.destroy)
-#boton2= tkinter.Button(app,text='print',width=25,command= lambda : printear("holaa"))
-testo2=tkinter.Text(app,width=50,height=10)
-testo2.grid(row=0,column=0,sticky=tkinter.W,columnspan=2)
+#AT+CMGF
+#AT+CMGR=INDEX
+#AT+CPMS="SM"
+#cuadro de mensajes
+cuadro=tkinter.Text(writeFrame,width=50,height=10)
+cuadro.grid(row=0,column=0,sticky=tkinter.N,columnspan=2,rowspan=10)
+t=threading.Thread(target=constantCheck,daemon=True)
+tkinter.Button(master=comFrame,text='connect',command= startPorts ).grid(
+    row=0,column=1)
+
+#tkinter.Scrollbar(master=comFrame).grid(column=2,rowspan=10)
+
+writeFrame.grid(row=0,column=0,sticky=tkinter.N)
+comFrame.grid(row=0,column=1)
+telFrame.grid(row=0,column=2,sticky=tkinter.W)
 const=1
-for boton in butons:
-    tkinter.Label(master=app,text='COM'+boton['text']).grid(row=const,column=0,sticky=tkinter.W)
-    boton['text']='Status: OFFLINE'
+for boton in statuses:
+    checkBoxes.append(0)
+    checkBoxes[const-1]=tkinter.IntVar()
+    tkinter.Checkbutton(master=comFrame,text='COM'+boton['text'],height=1
+        ,variable=checkBoxes[const-1],onvalue=1,offvalue=0).grid(row=const,column=0,sticky=tkinter.W)
+    
+    telefonos.append(tkinter.Entry(master=telFrame,width=10,bd=4))
+    telefonos[const-1].grid(row=const,column=2,sticky=tkinter.W)
+
+    boton['text']=puertos[0].status
     boton.grid(row=const,column=1,sticky=tkinter.W)
     const+=1
-#boton1.grid(row=1,column=0,sticky=tkinter.W)
-#boton2.grid(row=1,column=1,sticky=tkinter.E)
 
+tkinter.Button(master=comFrame,text='Dial',command= crossDial ).grid(
+    row=const,column=1)
+
+t.start()
 app.mainloop()
+
